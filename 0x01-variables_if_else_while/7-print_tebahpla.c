fcc03205d4c0b5d3db8dc3ad2a306216786c2741
joeabdelghanii <yelsayed392@gmail.com>
2023-03-17 17:47:07 -0700

    l

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+* main - prints if number is +
+*
+* Return: Always (Success)
+*/
+
+int main(void)
+{
+char ch;
+
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
