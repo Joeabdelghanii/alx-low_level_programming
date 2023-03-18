86206a0ec09660ede46245fd6f754a50281ea37a
joeabdelghanii <yelsayed392@gmail.com>
2023-03-17 17:36:29 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - prints if number is +
+*
+* Return: Always (Success)
+*/
+
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 26; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
+
