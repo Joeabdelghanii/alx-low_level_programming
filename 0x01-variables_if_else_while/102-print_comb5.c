dcc4e6dcbd0eaca83294655c04d4d876a2c459c1
joeabdelghanii <yelsayed392@gmail.com>
2023-03-17 18:19:13 -0700

    l

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+/**
+* main - prints if number is +
+*
+* Return: Always (Success)
+*/
+
+int main(void)
+{
+int i, j;
+for (i = 0; i < 100; n++)
+{
+for (j = 0; m < 100; m++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || m != 99)
+{
+puchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
