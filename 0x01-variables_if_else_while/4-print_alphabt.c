15268bfd136e4c82ab435828618a255ae94d7f84
joeabdelghanii <yelsayed392@gmail.com>
2023-03-17 17:41:37 -0700

    l

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* main - prints if number is +
+*
+* Return: Always (Success)
+*/
+
+int main(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+if (i != 101 && i != 113)
+{
+putchar(i)
+}
+}
+putchar('\n');
+return (0);
+}
+
