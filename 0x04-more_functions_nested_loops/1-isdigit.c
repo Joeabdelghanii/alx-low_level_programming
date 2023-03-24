e6bad9c021afc810143510724e69bcffb6163e74
joeabdelghanii <yelsayed392@gmail.com>
2023-03-23 18:24:10 -0700

    1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
 *_isupper - check if a letter is upper
-*@x: The number to be checked
+*@c: The number to be checked
 *Return: 1 for upper letter
 */
 int _isupper(int c)
 {
-if (c >= '0' && x <= '0')
+if (c >= '0' && c <= '0')
 {
 return (1);
 }
