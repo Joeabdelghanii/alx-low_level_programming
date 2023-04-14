f410129aecfb3b33c3c3ac19c6acff1e6f4320c7
joeabdelghanii <yelsayed392@gmail.com>
2023-04-14 09:23:01 -0700

    l

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -16,12 +16,12 @@ int count_word(char *s)
 
 	for (c = 0; s[c] != '\0'; c++)
 	{
-	if (s[c] == ' ')
+		if (s[c] == ' ')
 			flag = 0;
-	else if (flag == 0)
+		else if (flag == 0)
 		{
-		flag = 1;
-		w++;
+			flag = 1;
+			w++;
 		}
 	}
 
@@ -55,16 +55,16 @@ char **strtow(char *str)
 		{
 			if (c)
 			{
-			end = i;
-			tmp = (char *) malloc(sizeof(char) * (c + 1));
-			if (tmp == NULL)
-				return (NULL);
-			while (start < end)
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
 					*tmp++ = str[start++];
-			*tmp = '\0';
-			matrix[k] = tmp - c;
-			k++;
-			c = 0;
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
 			}
 		}
 		else if (c++ == 0)
@@ -75,4 +75,3 @@ char **strtow(char *str)
 
 	return (matrix);
 }
-
